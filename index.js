cb9bf52d6ec4ff691b58bdf67daed9ff42fce490
marco.foschian <marco.foschian@gmail.com>
2020-10-18 22:00:57 +0200

    experiments with auth

--- index.js
+++ index.js
@@ -24,4 +24,45 @@ bot.hears(/hi/i, (ctx) => ctx.reply('Hey there'))
 bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('modern', ({ reply }) => reply('Yo'))
 bot.command('hipster', Telegraf.reply('λ'))
+
+
+function dump( ctx, field ) {
+	let info = ctx[field];
+	if( info ) {
+		console.log( '%s:', field );
+		console.table( info );
+	}
+}
+
+
+bot.command('info', (ctx) =>  {
+	dump( ctx, 'chat' );
+	dump( ctx, 'from' );
+	ctx.reply('ok')
+})
+
+let Auth = {
+	_users: [ 211468854 ],
+	allow: (x) => Auth._users.indexOf(x) >= 0
+};
+
+bot.command('priv', (ctx) =>  {
+	console.log( 'priv' );
+	let uid = ctx.from;
+
+	if( ctx.from ) {
+		console.log( 'from id: %s', ctx.from.id );
+		if( Auth.allow(ctx.from.id) ) {
+			console.log( 'ok' );
+			ctx.reply('ok');
+			return;
+		}
+	}
+
+	console.log( 'volentieri' );
+	ctx.reply('volentieri');
+})
+
+
+
 bot.launch()
